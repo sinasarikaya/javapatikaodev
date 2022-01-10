df62b7468c887d625fbb541ee7baedd35eed7f66
Sina Sarıkaya <54350240+sinasarikaya@users.noreply.github.com>
2022-01-10 17:26:43 +0300

    Add files via upload

---
+++ ogrencibilgi/Student.java
@@ -0,0 +1,88 @@
+package ogrencibilgi;
+ class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void addBulkVivaNote(int mat, int fizik, int kimya){
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.vivanote = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.vivanote = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.vivanote = kimya;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = ((this.fizik.note*0.9 + this.fizik.vivanote*0.1) + (this.kimya.note*0.8+this.kimya.vivanote*0.2) + (this.mat.note*0.7+this.mat.vivanote*0.3)) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+}
---
+++ ogrencibilgi/asd.java
@@ -0,0 +1,4 @@
+package ogrencibilgi;
+
+public class asd {
+}
