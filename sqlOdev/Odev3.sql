ee1c7e0377c1f842dd970f30aa6937c04ade7730
Sina Sarıkaya <54350240+sinasarikaya@users.noreply.github.com>
2022-03-08 21:38:44 +0300

    Create Odev3.sql

---
+++ sqlOdev/Odev3.sql
@@ -0,0 +1,4 @@
+-- 1 SELECT country FROM country WHERE country LIKE 'A%a';
+-- 2 SELECT country FROM country WHERE country LIKE '______n';
+-- 3 SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+-- 4 SELECT * FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
